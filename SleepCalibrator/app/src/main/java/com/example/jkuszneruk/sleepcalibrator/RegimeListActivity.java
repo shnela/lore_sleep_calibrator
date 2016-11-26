2e6542708063546586369283ea3c9ffa759c313e
Vladimir Smirnov <synteny@users.noreply.github.com>
2016-11-27 02:52:17 +0300

    Fixing last commit

--- SleepCalibrator/app/src/main/java/com/example/jkuszneruk/sleepcalibrator/RegimeListActivity.java
+++ SleepCalibrator/app/src/main/java/com/example/jkuszneruk/sleepcalibrator/RegimeListActivity.java
@@ -1,5 +1,7 @@
 package com.example.jkuszneruk.sleepcalibrator;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
